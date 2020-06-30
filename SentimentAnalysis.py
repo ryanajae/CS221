5203bf5a2ab752936a0a6aa98417d08cb1853f5b
Ryan Fong Jae <ryanjae@cs.stanford.edu>
2020-06-29 17:20:28 -0700

    Create SentimentAnalysis.py

---
+++ SentimentAnalysis.py
@@ -0,0 +1,99 @@
+#!/usr/bin/python
+
+import random
+import collections
+import math
+import sys
+from util import *
+
+############################################################
+# Problem 3: binary classification
+############################################################
+
+############################################################
+# Problem 3a: feature extraction
+
+def extractWordFeatures(x):
+    """
+    Extract word features for a string x. Words are delimited by
+    whitespace characters only.
+    @param string x: 
+    @return dict: feature vector representation of x.
+    Example: "I am what I am" --> {'I': 2, 'am': 2, 'what': 1}
+    """
+    # BEGIN_YOUR_CODE (our solution is 4 lines of code, but don't worry if you deviate from this)
+    words = x.split()
+    features = dict()
+    for word in words:
+    	appearances = words.count(word)
+    	features.update({word : appearances})
+    return features
+    # END_YOUR_CODE
+
+############################################################
+# Problem 3b: stochastic gradient descent
+
+def learnPredictor(trainExamples, testExamples, featureExtractor, numIters, eta):
+    '''
+    Given |trainExamples| and |testExamples| (each one is a list of (x,y)
+    pairs), a |featureExtractor| to apply to x, and the number of iterations to
+    train |numIters|, the step size |eta|, return the weight vector (sparse
+    feature vector) learned.
+
+    You should implement stochastic gradient descent.
+
+    Note: only use the trainExamples for training!
+    You should call evaluatePredictor() on both trainExamples and testExamples
+    to see how you're doing as you learn after each iteration.
+    '''
+    weights = {}  # feature => weight
+    # BEGIN_YOUR_CODE (our solution is 12 lines of code, but don't worry if you deviate from this)
+    for i in xrange(numIters - 1):
+        for (x, y) in trainExamples:
+            features = featureExtractor(x)
+            wPhiY = dotProduct(features, weights) * y
+            if wPhiY < 1:
+                increment(weights, eta * y, features)
+    # END_YOUR_CODE
+    return weights
+
+############################################################
+# Problem 3c: generate test case
+
+def generateDataset(numExamples, weights):
+    '''
+    Return a set of examples (phi(x), y) randomly which are classified correctly by
+    |weights|.
+    '''
+    random.seed(42)
+    # Return a single example (phi(x), y).
+    # phi(x) should be a dict whose keys are a subset of the keys in weights
+    # and values can be anything (randomize!) with a nonzero score under the given weight vector.
+    # y should be 1 or -1 as classified by the weight vector.
+    def generateExample():
+        # BEGIN_YOUR_CODE (our solution is 2 lines of code, but don't worry if you deviate from this)
+        phi = { random.choice(weights.keys()) : random.randint(1, 6) } #Wasn't sure if some weight could be nonzero
+        y = 1 if dotProduct(phi, weights) >= 0 else -1 #Originally I just called random.choice(weights.values())
+        # END_YOUR_CODE
+        return (phi, y)
+    return [generateExample() for _ in range(numExamples)]
+
+############################################################
+# Problem 3e: character features
+
+def extractCharacterFeatures(n):
+    '''
+    Return a function that takes a string |x| and returns a sparse feature
+    vector consisting of all n-grams of |x| without spaces.
+    EXAMPLE: (n = 3) "I like tacos" --> {'Ili': 1, 'lik': 1, 'ike': 1, ...
+    You may assume that n >= 1.
+    '''
+    def extract(x):
+        # BEGIN_YOUR_CODE (our solution is 6 lines of code, but don't worry if you deviate from this)
+        x = ''.join(x.split())
+        nGrams = dict()
+        for i in xrange(len(x) - n + 1):
+            nGrams.update({ x[i:i+n] : 1 })
+        return nGrams
+        # END_YOUR_CODE
+    return extract
